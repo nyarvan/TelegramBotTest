278a9f933deed1bb6602fcee71ce23e356dee18c
Ваня Рыбак <infinity.man102@gmail.com>
2021-11-24 20:47:07 +0200

    telegrambot

---
+++ bot_polling.py
@@ -0,0 +1,82 @@
+from config import TOKEN
+import telebot
+
+bot = telebot.TeleBot(TOKEN)
+
+
+
+with open("courses.txt") as file:
+    courses = [item.split(",") for item in file]
+
+with open("schedule.txt") as file:
+    schedule = {
+        'start': [],
+        'pro': [],
+        'other': []
+    }
+    for string in file:
+        if "start" in string.lower():
+            schedule["start"].append(string)
+        elif 'pro' in string.lower():
+            schedule["pro"].append(string)
+        else:
+            schedule["other"].append(string)
+
+
+@bot.message_handler(commands=["start"])
+def message_start(message):
+    bot.send_message(message.chat.id, "Hello, user!")
+
+@bot.message_handler(commands=["help"])
+def message_help(message):
+    res = "/courses - список ближайших курсов\n/schedule - расписание курсов"
+    bot.reply_to(message, res)
+
+@bot.message_handler(commands=["courses"])
+def list_courses(message):
+    keyboard = telebot.types.InlineKeyboardMarkup(row_width=1)
+
+    for text, url in courses:
+        url_button = telebot.types.InlineKeyboardButton(text=text.strip(), url=url.strip(' \n'))
+        keyboard.add(url_button)
+
+    bot.send_message(message.chat.id, 'A list of courses:', reply_markup=keyboard)
+
+@bot.message_handler(commands=["schedule"])
+def schedule_courses(message):
+    res = 'Schedule of courses:\n\n'
+
+    for category in schedule:
+        for item in schedule[category]:
+            title, date = item.split(",")
+            res += f"<b>{title}</b>: <code>{date}</code>"
+        res += "\n"
+
+    bot.send_message(message.chat.id, text=res, parse_mode='HTML')
+
+@bot.message_handler(func=lambda x: x.text.startswith('info'))
+def info_courses(message):
+    text_from_user = message.json['text']
+    res = ''
+    if 'python' in text_from_user.lower():
+
+        for category in schedule:
+            for item in schedule[category]:
+                title, date = item.split(",")
+                if "python" in title.lower():
+                    res += f"<b>{title}</b>: <code>{date}</code>"
+        res += "\n"
+        bot.send_message(message.chat.id, text=res, parse_mode="HTML")
+    elif "java" in text_from_user.lower():
+        for category in schedule:
+            for item in schedule[category]:
+                title, date = item.split(",")
+                if "java" in title.lower():
+                    res += f"<b>{title}</b>: <code>{date}</code>"
+        res += "\n"
+        bot.send_message(message.chat.id, text=res, parse_mode="HTML")
+    else:
+        bot.send_message(message.chat.id, f"I don`t understand you, {message.chat.id}")
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True, interval=0)
